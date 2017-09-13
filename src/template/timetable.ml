85a5f8341fd618185a0de0d29609192eac6edab6
Bertrand Desmons <bertrand.desmons@gmail.com>
2017-09-13 20:31:01 +0200

    Timetable setting for schoolyear 17-18 (at least, up to December, up to future updates).

--- src/template/timetable.ml
+++ src/template/timetable.ml
@@ -1,5 +1,5 @@
 let year1 = 2017
-let year2 = 2017
+let year2 = 2018
 
 type recurrence =
     Every_day
@@ -8,6 +8,10 @@ type recurrence =
   | Every_2weeks
   | Every_1st of Date.day
 
+type period_bound =
+    Start
+  | End
+
 let of_rec = function
     Every_day -> Date.next ?d:None
   | Every_(d) -> Date.next ~d
@@ -15,6 +19,12 @@ let of_rec = function
   | Every_2weeks -> fun date -> Date.next_week (Date.next_week date)
   | Every_1st(d) -> Date.next_m ~d
 
+let guaranteed_start = function
+    Every_day
+  | Every_month
+  | Every_2weeks -> true
+  | Every_ _
+  | Every_1st _ -> false
 
 type time = Date.t * Date.period
 
@@ -36,7 +46,11 @@ let set recur ~from ~till ?(except=[]) period =
       in
       create (if jump then exns' else exns)
         (if jump then list else date::list) (f date)
-  in create (List.map Date.make except) [] (f (Date.prev from))
+  in
+  let start_date =
+    if guaranteed_start recur then from
+    else f (Date.prev from)
+  in create (List.map Date.make except) [] start_date
 
 type t =
 {ewhat: string;
@@ -92,13 +106,65 @@ let list_merge lists =
           constructor d [(p,q,o)] ((date, current)::list)
   in constructor (Date.make (Date.Jan 1, 2017)) [] []
 
-let mk_fresnes d h ?(jun=false)?(c=126) n s =
+(*let mk_fresnes d h ?(jun=false)?(c=126) n s =
   let min = if h > 12 then 30 else 0 in
   let time = Printf.sprintf "%02d:%02d-%02d:%02d" h min (h+1) min in
   let line = Printf.sprintf "Cours (%de %d; A%03d)" n s c in
-  make line "Fresnes-sur-Escaut" Date.([single (if jun then Jun d else May d) time])
+  make line "Fresnes-sur-Escaut" Date.([single (if jun then Jun d else May d) time]) *)
 
-let jun = true
+let periods =
+  Date.([(Sep 1, 2017), (Oct 20, 2017);
+   (Nov 6, 2017), (Dec 22, 2017);
+   (Jan 8, 2018), (Feb 19, 2018);
+   (Mar 5, 2018), (Apr 20, 2018);
+   (May 7, 2018), (Jul 6, 2018)])
+
+let labbe_time =  function
+    8, Start -> "08:05"
+  | 8, End
+  | 9, Start -> "09:00"
+  | 9, End -> "09:55"
+  | 10, Start -> "10:10"
+  | 10, End
+  | 11, Start -> "11:05"
+  | 11, End
+  | 12, Start -> "12:00"
+  | 12, End
+  | 13, Start -> "12:55"
+  | 13, End -> "13:50"
+  | 14, Start -> "13:55"
+  | 14, End
+  | 15, Start -> "14:50"
+  | 15, End -> "15:45"
+  | 16, Start -> "16:00"
+  | 16, End
+  | 17, Start -> "16:55"
+  | 17, End -> "17:50"
+  | t, _ -> invalid_arg (string_of_int t)
+
+let labbe_1 d h =
+  let time =
+    Printf.sprintf "%s-%s" (labbe_time (h, Start)) (labbe_time (h, End))
+  in
+  let merger l (from, till) =
+    List.rev_append (Date.(set (Every_ d) ~from ~till time)) l
+  in
+  List.fold_left merger [] periods
+
+let labbe classe list =
+  let rec aux res = function
+      [] -> res
+    | (d, h) :: l ->
+       let list = labbe_1 d h in
+       aux (List.rev_append list res) l
+  in
+  let line = Printf.sprintf "Cours (%s)" classe in
+  make line "Douai" (aux [] list)
+   
+let form ue dates =
+  make (Printf.sprintf "Formation (%s)" ue) "Valenciennes" dates
+
+(*let jun = true*)
 
 let t =
   let l = List.map (fun event ->
@@ -107,7 +173,8 @@ let t =
                                d, p,
                                event.ewhat, event.ewhere)
                               event.ewhen))
-		   [make "Non dispo" "Fresnes-sur-Escaut"
+		   [
+		     (*(make "Non dispo" "Fresnes-sur-Escaut"
 			 Date.([single (May 9) "08:30-11:00"]);
 		    mk_fresnes 9 11 ~c:118 5 1;
 		    mk_fresnes 10 10 3 1;
@@ -175,17 +242,17 @@ let t =
 		    mk_fresnes ~jun 14 9 5 1 ~c:118;
 		    mk_fresnes ~jun 14 10 3 1;
 		    mk_fresnes ~jun 14 11 6 1;
-		    (*mk_fresnes ~jun 15 10 6 1;
+		    ( *mk_fresnes ~jun 15 10 6 1;
 		    mk_fresnes ~jun 15 11 6 2;
 		    mk_fresnes ~jun 16 8 3 1;
 		    mk_fresnes ~jun 16 9 6 2;
-		    mk_fresnes ~jun 16 10 6 1;*)
+		    mk_fresnes ~jun 16 10 6 1;* )
 		    mk_fresnes ~jun 19 8 6 1;
 		    mk_fresnes ~jun 19 10 6 2;
 		    mk_fresnes ~jun 19 11 6 2;
 		    mk_fresnes ~jun 19 13 3 3 ~c:218;
 		    mk_fresnes ~jun 19 14 3 1;
-		  (*  mk_fresnes ~jun 20 10 6 2;
+		  ( *  mk_fresnes ~jun 20 10 6 2;
 		    mk_fresnes ~jun 20 11 5 2 ~c:118;
 		    mk_fresnes ~jun 21 10 3 1;
 		    mk_fresnes ~jun 21 11 6 1;
@@ -193,31 +260,63 @@ let t =
 		    mk_fresnes ~jun 22 11 6 2;
 		    mk_fresnes ~jun 23 8 3 1;
 		    mk_fresnes ~jun 23 9 6 2;
-		    mk_fresnes ~jun 23 10 6 1;*)
-		    cp "Nomain"
-		       Date.( []);
-		    cag "Valenciennes" [];
-		    cp "Fresnes-sur-Escaut"
-		       Date.([]);
-		    cp "Faumont"
-		       Date.([]);
-		    cp "Flines-lez-Râches"
-		       Date.([]);
-		    cp "Coutiches"
-		       Date.([] );
-		    cp "Saméon"
-		       Date.([]);
-		    cp "Nivelle"
-		       Date.([]);
-		    make "Concours CAPES" "Nancy"
-			 Date.(single (Jun 15) "00:00-23:59"
-			       :: single (Jun 16) "00:00-23:59"
-			       :: []);
-		    make "Concours Agreg" "Lille"
-			 Date.(single (Jun 22) "12:00-19:00"
-			       :: single (Jun 23) "08:00-19:00"
-			       :: single (Jun 24) "08:00-19:00"
-			       :: [])
+		    mk_fresnes ~jun 23 10 6 1;* ) *)
+		     cag "Valenciennes" [];
+
+		     
+		     cp "Fresnes-sur-Escaut"
+			Date.(set (Every_ Sat)
+				  (Aug 28, 2017)
+				  (Jun 15, 2018) "09:00-11:00");
+		     cp "Flines-lez-Râches"
+			Date.(set (Every_ Fri)
+				  (Sep 29, 2017)
+				  (Jun 15, 2018) "17:30-19:30");
+		     labbe "2 6" Date.([Mon, 15; Tue, 10; Fri, 11]);
+		     labbe "1 STMG1" Date.([Mon, 14; Tue, 9; Fri, 10]);
+		     labbe "2 6 - 2" Date.([Mon, 8]);
+		     labbe "2 6 - 1" Date.([Mon, 9]);
+
+		     form "LV"
+			  Date.(set Every_2weeks
+				    (Sep 13, 2017)
+				    (Oct 11, 2017) "10:00-12:00");
+		     form "TICE"
+			  Date.([single (Sep 20) "13:30-14:30";
+				 single (Nov 29) "13:30-15:30"]);
+		     form "HisMaths"
+			  Date.(single (Nov 8) "10:00-12:00"
+				:: single (Nov 15) "10:00-12:00"
+				:: set Every_2weeks
+				    ~from: (Sep 20, 2017)
+				    ~till: (Nov 29, 2017)
+				    ~except:[Nov 1, 2017;
+					     Nov 15, 2017] "10:00-12:00");
+		     form "Didac"
+			  Date.(single (Sep 14) "09:00-12:00"
+				:: single (Oct 5) "09:00-12:00"
+				:: single (Nov 23) "09:00-12:00"
+				:: single (Dec 7) "10:00-12:00"
+				:: single (Dec 13) "10:00-12:00"
+				:: []);
+		     
+		     form "Rech"
+			  Date.([single (Sep 13) "13:30-14:00"]);
+
+		     form "ContEx"
+			  (List.map (fun d -> single d "09:00-16:00")
+				    Date.([Sep 7;
+					   Sep 21;
+					   Oct 12;
+					   Nov 16;
+					   Nov 30;
+					   Dec 21]));
+		     form "SitPro"
+			   (List.map (fun d -> single d "13:30-16:00")
+				    Date.([Sep 6;
+					   Oct 11;
+					   Nov 15;
+					   Dec 13]));
 		   ]
   in
   list_merge l
